94cd34bb668ae8192cfcfa12f089b5d433b0d32b
agsNeto20 <37492761+agsNeto20@users.noreply.github.com>
2020-11-27 11:31:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,21 @@ import database
 def menu():
 
     menu_interface = """-- Netflox --
-    1) Sign Up.
-    2) Log in.
-    0) Exit.
+    1) Sign Up
+    2) Log in
+    0) Exit
     Your Selection: """
 
-    while (user_input := input(menu_interface)) != "0":
-        if user_input == "1":
+    while (user_input := input(menu_interface)) != '0':
+
+        if user_input == '1':
             print("Sign up")
             name = input("Enter your name: ")
             email = input("Enter your e-mail: ")
             password = input("Enter your password: ")
             database.create_user(name, email, password)
 
-        elif user_input == "2":
+        elif user_input == '2':
             print("Log in")
             email = input("Enter your e-mail: ")
             password = input("Enter your password: ")
@@ -30,25 +31,36 @@ def menu():
 
 def client():
 
-    client_interface = """Welcome
-        1) Search products.
-        2) My history.
-        3) Messages.
+    client_interface = """
+        1) Search products
+        2) My history
+        3) Messages
         0) Exit
         Your Selection: """
 
-    while (user_input := input(client_interface)) != "0":
-        if user_input == "1":
+    while (client_input := input(client_interface)) != '0':
+
+        if client_input == '1':
 
             search_interface = """Order by
-                    1) Name
-                    2) Actor
-                    3) Director
-                    4) Date
-                    5) 
-                    6)
-                    7)
-                    Your Selection: """
+                1) Name
+                2) Actor
+                3) Director
+                4) Date
+                5) IMDB rating
+                6) Genre
+                7) Price
+                0) Exit
+                Your Selection: """
+            
+            #range of dates
+            #range of prices
+
+            while (search_input := input(search_interface)) != '0':
+
+                if search_input == '1':
+                    name = input("Insert actor name: ")
+                    database.findby_name(name) #doesnt exist yet
 
 
 menu()
