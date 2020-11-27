a8f45c59d90a134e4063a46709b0890ab44f4ef5
agsNeto20 <37492761+agsNeto20@users.noreply.github.com>
2020-11-27 11:07:28 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,40 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=Netflox user=postgres password=postgres")
-
-CREATE_USER = "INSERT INTO users (adminid, clientid, name, email, password, balance) VALUES (?, ?, ?, ?, ?, ?);"
-
-adminid = 0
-clientid = 0
-
-def create_user(conn, adminid, clientid, name, email, password):
-    with conn:
-        conn.execute(CREATE_USER, (adminid, clientid, name, email, password, 20))
 
 
-conn.close()
+def create_user(name, email, password):
+    conn = psycopg2.connect("host=localhost dbname=Netflox user=postgres password=postgres")
+    c = conn.cursor()
+
+    c.execute("INSERT INTO users (nome, email, password, balance) VALUES ('"+name+"','"+email+"','"+password+"',20)")
+
+    conn.commit()
+    conn.close()
+
+
+def log_in(email, password):
+    conn = psycopg2.connect("host=localhost dbname=Netflox user=postgres password=postgres")
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM users WHERE email = '"+email+"' AND password = '"+password+"'")
+    results = c.fetchall()
+
+    conn.commit()
+    conn.close()
+
+    if results:
+        for i in results:
+            print("Welcome" + i[1])
+        return 1
+    else:
+        print("Email and password not recognised")
+        return 0
+
+
+def delete_user(userid):
+    conn = psycopg2.connect("host=localhost dbname=Netflox user=postgres password=postgres")
+    c = conn.cursor()
+
+    c.execute("DELETE FROM users WHERE userid = (" + userid + ")")
+
+    conn.commit()
+    conn.close()
