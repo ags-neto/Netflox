8b645024b45483d606db80e2e922ed8f5bc147b6
alexalmeida2001 <47391414+alexalmeida2001@users.noreply.github.com>
2020-12-07 17:29:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ def menu():
             password = input("Enter your password: ")
             if database.log_in(email, password):
                 database.view_saldo(email)
-                break
+                return(email)
 
         else:
             print("Invalid input, please try again")
-    return(email)
+
 
 def client():
     client_interface = """
@@ -47,60 +47,115 @@ def client():
                 1) Name
                 2) Actor
                 3) Director
-                4) Date
-                5) IMDB rating
-                6) Genre
-                7) Price
+                4) Type
                 0) Exit
                 Your Selection: """
 
-            # range of dates
-            # range of prices
-
             while (search_input := input(search_interface)) != '0':
-
                 if search_input == '1':
-                    name = input("Insert actor name: ")
-                    database.findby_name(name)  # doesnt exist yet
+                    name = input("Insert movie name: ")
+                    database.findby_name(name)
+                if search_input == '2':
+                    actor = input("Insert actor name: ")
+                    database.findby_actor(actor)
+
+                if search_input == '4':
+                    type = input("Insert 'movie' or 'series': ")
+                    database.findby_type(type)
+
+                if search_input == '3':
+                    director = input("Insert Director name: ")
+                    database.findby_director(director)
+
+                if search_input == '4':
+                    type = input("Insert 'movie' or 'series': ")
+                    database.findby_type(type)
+
+
         if client_input  == '2':
             database.view_allmovies()
-            client_interface_view = "\nselecione um filme ou pressione zero para saír "
-            while (client_input_view := input(client_interface_view)) !='0':
-                if client_input_view != '0':
-                    print("id  name,  actorid, director, imdbrating, genre, price, year, timeavaible, type")
-                    moviename=database.view_movieinfo(client_input_view)
-                    if moviename==0:
-                        break
-                    client_input_view_buy =input ("press 1 to buy press 0 to leave\n")
+            client_input_view = input("Select a movie or\nOrder by: 1.Title 2. Director 3.Date of release 4.Imdb rating 0.Go back\n")
+            if client_input_view == '1':
+                print("ordered by title:\n")
+                database.view_allmovies_ordertitle()
+
+            if client_input_view == '2':
+                print("ordered by Director:\n")
+                database.view_allmovies_orderdirector()
+            if client_input_view == '3':
+                print("ordered by Date of Realease:\n")
+                database.view_allmovies_orderdate()
+            if client_input_view == '4':
+                print("ordered by Imdb rating:\n")
+                database.view_allmovies_orderimdb()
+
+            if (client_input_view != '0') and (client_input_view != '1') and (client_input_view != '2') and (client_input_view != '3') and (client_input_view != '4') :
+                print("id  name,  actorid, director, imdbrating, genre, price, year, timeavaible, type")
+                moviename=database.view_movieinfo(client_input_view)
+                if moviename==0:
+                     break
+                client_input_view_buy =input ("press 1 to buy press 0 to leave\n")
+                if client_input_view_buy == '1':
+                    client_input_view_buy=input("are you sure you want to buy?\n(press 1 yes press 0 no)")
+                    print("\no seu saldo é :")
+                    saldo=database.view_saldo(email)
+                    print(" o item custa:")
+                    custo=database.view_custo(client_input_view)
+                    client_input_view_buy = input("press 1 to confirm press 0 to leave\n")
                     if client_input_view_buy == '1':
-                        client_input_view_buy=input("are you sure you want to buy?\n(press 1 yes press 0 no)")
-                        print("\no seu saldo é :")
-                        saldo=database.view_saldo(email)
-                        print(" o item custa:")
-                        custo=database.view_custo(client_input_view)
-                        client_input_view_buy = input("press 1 to confirm press 0 to leave\n")
-                        if client_input_view_buy == '1':
-                            if(custo<saldo):
-                               print("item bought successefully!\n")
-                               database.compra_filme(custo,saldo,email)
-                               database.create_rent(custo,email,moviename)
-                               print("saldo atual:\n")
-                               database.view_saldo(email)
-                            else:
-                                print("nao tem saldo suficiente")
+                        if(custo<saldo):
+                            print("item bought successefully!\n")
+                            database.compra_filme(custo,saldo,email)
+                            database.create_rent(custo,email,moviename)
+                            print("saldo atual:\n")
+                            database.view_saldo(email)
+                        else:
+                            print("nao tem saldo suficiente")
         if client_input == '3':
             database.view_avaible_movies(email)
-
+            client_input_available_movies=input("Order by: 1.Title 2. Director 3.Date of release 4.Imdb rating 0.Go back\n")
+            if client_input_available_movies == '1':
+                print("ordered by title:\n")
+                #database.view_availablemovies_ordertitle(email)
+            if client_input_available_movies == '2':
+                print("ordered by Director:\n")
+                database.view_availablemovies_orderdirector(email)
+            if client_input_available_movies == '3':
+                print("ordered by Date of Realease:\n")
+                #database.view_availablemovies_orderdate(email)
+            if client_input_available_movies == '4':
+                print("ordered by Imdb rating:\n")
+                #database.view_availablemovies_orderimdb(email)
 
 
         if client_input  == '4':
             print("clientid, date start,                                   price,                     date end,                 usermail,months, id, type, movieid")
             database.view_rent(email)
 
+        if client_input == '5':
+            message_interface = """
+                1) View unread messages
+                2) View read messages
+                0) Exit
+                Your Selection: """
+            while (client_input := input(message_interface)) != '0':
 
+                if client_input == '1':
+                    y = database.show_unread_messages(USERID)
+                    while (x := int(input("\tYour Selection: "))) != 0:
+                        database.read_message(y[x - 1][0])
+                        print("\n\t" + y[x - 1][1])
+                        print("\n\t0) Go back")
+
+                if client_input == '2':
+                    y = database.show_read_messages(USERID)
+                    while (x := int(input("\tYour Selection: "))) != 0:
+                        print("\n\t" + y[x - 1][1])
+                        print("\n\t0) Go back")
 
 
 
 email=menu()
+#USERID=menu()
 
 client()
